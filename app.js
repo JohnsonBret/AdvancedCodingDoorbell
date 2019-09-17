7ea68ed6f0cbb9e43673b5092b862d85194526e4
Bret <bretaudio@gmail.com>
2019-09-17 12:53:43 -0700

    Monday 9/17 Kid Genious

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require("express");
 
+var player = require('play-sound')(opts = {});
 var path = require('path');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.status(200).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/upstairs', (req, res)=>{
+    res.status(200).sendFile(path.join(__dirname, 'upstairs.html'));
+});
+
+app.post('/ring', (req, res)=>{
+    // console.log(req);
+    console.log(`Got your post -> Ring ${req.body.userName}`);
+
+    player.play('./public/sounds/doorbell-7.mp3', (err)=>{
+        if(err) throw err;
+    });
 });
 
 app.listen(3000, ()=>{
--- public/doorbell.js
+++ public/doorbell.js
@@ -7,5 +7,28 @@ enterButton.addEventListener("click", ()=>{
     formCont.style.display = "none";
 
     let ringo = document.getElementById("ringer");
-    ringo.style.display = "inline";
+    ringo.style.display = "flex";
+}); 
+
+let ringerButton = document.getElementById("ringerBtn");
+
+ringerButton.addEventListener("click", async ()=>{
+    console.log("Clicked Ringer");
+
+    let inputValue = document.getElementById("userName").value;
+
+    console.log(inputValue);
+    //Fetch API is AJAX!
+    const rawResponse = await fetch('/ring', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            userName: inputValue
+        })
+    });
+    // const content = await rawResponse.json()
 });
+  
